608e21487cfff048a6dbe299a66f2ee841a2b62a
Bahaand Wardak <bahaandwardak@gmail.com>
2026-09-07 15:36:41 +0100

    Change statistics

--- display_event_statistics.py
+++ display_event_statistics.py
@@ -1,29 +1,8 @@
-events = [
-    {
-        "name": "Painting Workshop",
-        "date": "2026-09-12",
-        "event_type": "Workshop",
-        "capacity": 10,
-        "attendees": ["Quad Azizi", "Bahaand Wardak"]
-    },
-    {
-        "name": "7-a-side Football",
-        "date": "2026-09-20",
-        "event_type": "Sports",
-        "capacity": 14,
-        "attendees": ["Quresh Alshammari"]
-    },
-    {
-        "name": "Charity Bake Sale",
-        "date": "2026-10-01",
-        "event_type": "Fundraiser",
-        "capacity": 30,
-        "attendees": ["Luke Parry"]
-    }
-]
+from data import events
 
 total_registrations = 0
 total_capacity = 0
+
 for event in events:
     total_registrations += len(event["attendees"])
     total_capacity += (event["capacity"])
