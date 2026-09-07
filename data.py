6b7424f84759552887ce52d722eed3dc34c5b1ff
Bahaand Wardak <bahaandwardak@gmail.com>
2026-09-07 15:53:54 +0100

    Changed data
    :q

--- data.py
+++ data.py
@@ -4,20 +4,20 @@ events = [
         "date": "2026-09-12",
         "event_type": "Workshop",
         "capacity": 10,
-        "attendees": ["Quad Azizi", "Bahaand Wardak"]
+        "attendees": ["Quad Azizi", "Bahaand Wardak", "John Doe", "John Smith"]
     },
     {
         "name": "7-a-side Football",
         "date": "2026-09-20",
         "event_type": "Sports",
         "capacity": 14,
-        "attendees": ["Quresh Alshammari"]
+        "attendees": ["Quresh Alshammari", "Jane Doe"]
     },
     {
         "name": "Charity Bake Sale",
         "date": "2026-10-01",
         "event_type": "Fundraiser",
         "capacity": 30,
-        "attendees": ["Luke Parry"]
+        "attendees": ["Luke Parry", "John Quincy Adams", "Tony Adams"]
     }
 ]
